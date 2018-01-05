07fc5876b5f3989b5fb64ba81a862922dba31c6b
Nishit Tated <nishittated@gmail.com>
2018-01-05 17:00:25 +0530

    Add files via upload

---
+++ src/dao/Dao.java
@@ -0,0 +1,50 @@
+/* Copyright (c) Nishit Tated. All Rights Reserved.
+ *  Project name: Online Election Voting System
+ * This project is licensed under the MIT License, see LICENSE.
+ */ 
+package dao;
+
+import java.sql.*;
+import model.Model;
+
+public class Dao {
+	static Connection con = null; 
+	static    
+	{
+		try 
+		{
+			Class.forName("com.ibm.db2.jcc.DB2Driver");
+		    con = DriverManager.getConnection("jdbc:db2://localhost:50000/OEVS", "admin", "lappipassword");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static int register(Model m, String sql) throws SQLException {
+		PreparedStatement ps = con.prepareStatement(sql);
+		int i = ps.executeUpdate();
+		return i;
+	}
+	public static ResultSet validate(Model m, String sql) throws SQLException {	
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	
+	public static int register1(String sql) throws SQLException {
+		PreparedStatement ps = con.prepareStatement(sql);
+		int i = ps.executeUpdate();
+		return i;
+	}
+	public static ResultSet validate1(String sql) throws SQLException {	
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	public static int common(Model m, String sql,String update) throws SQLException {
+		PreparedStatement ps = con.prepareStatement(sql);
+		int i =ps.executeUpdate();	
+		return i;
+	} 
+}
