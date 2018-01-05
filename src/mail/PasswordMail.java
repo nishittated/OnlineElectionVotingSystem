830e174ec4e89cbbef6aefdeb2f5e98e932829ab
Nishit Tated <nishittated@gmail.com>
2018-01-05 17:01:18 +0530

    Add files via upload

---
+++ src/mail/PasswordMail.java
@@ -0,0 +1,145 @@
+/* Copyright (c) Nishit Tated. All Rights Reserved.
+ *  Project name: Online Election Voting System
+ * This project is licensed under the MIT License, see LICENSE.
+ */ 
+package mail;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+	public class PasswordMail {
+		public PasswordMail() {
+		}
+		
+		public boolean sendMail(String to,String otp) throws MessagingException 
+		{
+			String host="smtp.gmail.com";
+			String username="";//emailid
+			String password="";//emailid password
+			String from=" ";// email from which u have to send
+			String subject="One Time Password";
+			String body="Your One Time passsword is "+otp;
+	
+			boolean sessionDebug=false;
+			
+			Properties props=System.getProperties();
+			props.put("mail.host",host);
+			props.put("mail.transport.protocol","smtp");
+			props.put("mail.smtp.starttls.enable","true");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.debug", "true");
+			props.put("mail.smtp.socketFactory.port", "465");
+			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.socketFactory.fallback", "false");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "25"); 
+			
+			Session mailSession=Session.getDefaultInstance(props,null);
+			mailSession.setDebug(sessionDebug);
+			
+			Message msg=new MimeMessage(mailSession);
+			msg.setFrom(new InternetAddress(from));
+			InternetAddress [] address={new InternetAddress(to)};
+			msg.setRecipients(Message.RecipientType.TO,address);
+			msg.setSubject(subject);
+			msg.setSentDate(new Date());
+			msg.setText(body);
+
+			Transport tr=mailSession.getTransport("smtp");
+			tr.connect(host,username,password);
+			msg.saveChanges();
+			tr.sendMessage(msg,msg.getAllRecipients());
+			tr.close();
+			//Transport.send(msg);
+			return true;
+		}
+		public boolean sendMail1(String to,String passwrd, String link) throws MessagingException
+		{
+			String host="smtp.gmail.com";
+			String username="	 ";//emailid
+			String password="";//emailid password
+			String from=" ";// email from which u have to send
+			String subject="Website Password";
+			String body="Your website password is "+passwrd+"  Please click to reset "+link;
+			boolean sessionDebug=false;
+			
+			Properties props=System.getProperties();
+			props.put("mail.host",host);
+			props.put("mail.transport.protocol","smtp");
+			props.put("mail.smtp.starttls.enable","true");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.debug", "true");
+			props.put("mail.smtp.socketFactory.port", "465");
+			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.socketFactory.fallback", "false");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "25"); 
+	
+			Session mailSession=Session.getDefaultInstance(props,null);
+			mailSession.setDebug(sessionDebug);
+		
+			Message msg=new MimeMessage(mailSession);
+			msg.setFrom(new InternetAddress(from));
+			
+			InternetAddress [] address={new InternetAddress(to)};
+			msg.setRecipients(Message.RecipientType.TO,address);
+			msg.setSubject(subject);
+			msg.setSentDate(new Date());
+			msg.setText(body);
+			
+			Transport tr=mailSession.getTransport("smtp");
+			tr.connect(host,username,password);
+			msg.saveChanges();
+			tr.sendMessage(msg,msg.getAllRecipients());
+			tr.close();
+					//Transport.send(msg);
+			return true;
+		}
+		public boolean sendMail2(String to, String link) throws AddressException, MessagingException {
+	
+			String host="smtp.gmail.com";
+			String username="	 ";//emailid
+			String password="";//emailid password
+			String from=" ";// email from which u have to send
+			String subject="Website Password";
+			String body="Activation Link"+link+"Please click to reset "+link;
+			boolean sessionDebug=false;
+			
+			Properties props=System.getProperties();
+			props.put("mail.host",host);
+			props.put("mail.transport.protocol","smtp");
+			props.put("mail.smtp.starttls.enable","true");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.debug", "true");
+			props.put("mail.smtp.socketFactory.port", "465");
+			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.socketFactory.fallback", "false");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "25"); 
+			
+			Session mailSession=Session.getDefaultInstance(props,null);
+			mailSession.setDebug(sessionDebug);
+		
+			Message msg=new MimeMessage(mailSession);
+			msg.setFrom(new InternetAddress(from));
+		
+			InternetAddress [] address={new InternetAddress(to)};
+			msg.setRecipients(Message.RecipientType.TO,address);
+			msg.setSubject(subject);
+			msg.setSentDate(new Date());
+			msg.setText(body);
+		
+			Transport tr=mailSession.getTransport("smtp");
+			tr.connect(host,username,password);
+			msg.saveChanges();
+			tr.sendMessage(msg,msg.getAllRecipients());
+			tr.close();
+				//Transport.send(msg);
+			return true;
+		}
+	}
---
+++ src/model/Model.java
@@ -0,0 +1,159 @@
+/* Copyright (c) Nishit Tated. All Rights Reserved.
+ *  Project name: Online Election Voting System
+ * This project is licensed under the MIT License, see LICENSE.
+ */ 
+package model;
+
+import java.sql.*;
+import dao.Dao;
+public class Model {
+	private String fn;
+	private String mn;
+	private String ln;
+	private String gend;
+	private String con;
+	private String email;
+	private String add;
+	private String country;
+	private String state;
+	private String aadhar;
+	private String election;
+	private String pass;
+	private String dob;
+	private String subject;
+	private String message;
+	private String event;
+	private String party;
+	private String age;
+	private String pwork;
+	private String speaker;
+	private String city;
+	
+	public String getSpeaker() {
+		return speaker;
+	}
+	public void setSpeaker(String speaker) {
+		this.speaker = speaker;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEvent() {
+		return event;
+	}
+	public void setEvent(String event) {
+		this.event = event;
+	}
+
+	public String getParty() {
+		return party;
+	}
+	public void setParty(String party) {
+		this.party = party;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getPwork() {
+		return pwork;
+	}
+	public void setPwork(String pwork) {
+		this.pwork = pwork;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getFn() {
+		return fn;
+	}
+	public void setFn(String fn) {
+		this.fn = fn;
+	}
+	public String getMn() {
+		return mn;
+	}
+	public void setMn(String mn) {
+		this.mn = mn;
+	}
+	public String getLn() {
+		return ln;
+	}
+	public void setLn(String ln) {
+		this.ln = ln;
+	}
+	public String getGend() {
+		return gend;
+	}
+	public void setGend(String gend) {
+		this.gend = gend;
+	}
+	public String getCon() {
+		return con;
+	}
+	public void setCon(String con) {
+		this.con = con;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getAadhar() {
+		return aadhar;
+	}
+	public void setAadhar(String aadhar) {
+		this.aadhar = aadhar;
+	}
+	public String getElection() {
+		return election;
+	}
+	public void setElection(String election) {
+		this.election = election;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+}
