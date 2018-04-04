574ce14bdec96b94c7e8658cc0fd66fd9e79f8d4
Nishit Tated <nishittated@gmail.com>
2018-04-04 14:00:23 +0530

    Update OtpServlet.java

--- src/controller/OtpServlet.java
+++ src/controller/OtpServlet.java
@@ -4,7 +4,7 @@
  */ 
 package controller;
 
-import java.io.IOException;
+import java.io.IOException; 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
