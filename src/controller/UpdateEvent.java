85ed493982399b85e220eed6890a0b3e39440743
Nishit Tated <nishittated@gmail.com>
2018-04-22 23:54:07 +0530

    Update UpdateEvent.java

--- src/controller/UpdateEvent.java
+++ src/controller/UpdateEvent.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import model.Model;
 import dao.Dao;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet; 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
