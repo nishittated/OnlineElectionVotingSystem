3e8748dc9d72dae64f0cce06f7fafcb471fd3b91
Nishit Tated <nishittated@gmail.com>
2018-04-23 14:29:48 +0530

    Update AddCampaign.java

--- src/controller/AddCampaign.java
+++ src/controller/AddCampaign.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse; 
 import java.sql.*;
 import dao.Dao;
 import model.Model;
