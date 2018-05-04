eb51e545ef05296b897e8cca35ed61c2fc725575
Nishit Tated <nishittated@gmail.com>
2018-05-04 23:54:53 +0530

    Update CaptchaServlet.java

--- src/controller/CaptchaServlet.java
+++ src/controller/CaptchaServlet.java
@@ -7,7 +7,7 @@ package controller;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;  
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
